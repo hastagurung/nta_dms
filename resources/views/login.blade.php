77b792247cd2236dd5c6faa829cbd81780b912e3
devgr24 <gurungryu@gmail.com>
2022-07-01 00:05:30 +0545

    minor changes

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,7 +5,7 @@
 	<base href="../../../">
 	<meta charset="utf-8" />
 	<title>NTA | Document Mangement</title>
-	<meta name="description" content="Login page example" />
+	<meta name="description" content="Login page" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
 	<link href="{{asset('css/pages/login/classic/login-1.css')}}" rel="stylesheet" type="text/css" />
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -19,7 +19,7 @@ License: You must have a valid license purchased only from themeforest(the above
         <title>NTA | Document Management</title>
 
         {{-- Meta Data --}}
-        <meta name="description" content="@yield('page_description', $page_description ?? '')"/>
+        <meta name="description" content="Document Archival Software"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
 
         {{-- Favicon --}}
